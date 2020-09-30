1f5fb1e1d9ee9d1493522c441b6b9122ccca9482
Karl Floersch <karl@karlfloersch.com>
2020-09-30 13:04:41 -0400

    Begin fixing up sql tests

--- packages/integration-tests/test/deposit.spec.ts
+++ packages/integration-tests/test/deposit.spec.ts
@@ -1,12 +1,16 @@
 import './setup'
 import { Config } from '../src/config'
 
-import { Web3Provider, JsonRpcProvider } from '@ethersproject/providers'
+import { Provider, Web3Provider, JsonRpcProvider } from '@ethersproject/providers'
+import { Wallet } from '@ethersproject/wallet'
 import { ganache } from '@eth-optimism/ovm-toolchain'
+import { PostgresDB } from '@eth-optimism/core-db'
 import { OptimismProvider, sighashEthSign } from '@eth-optimism/provider'
 import { getContractFactory } from '@eth-optimism/rollup-contracts'
 import { getContractAddress } from '@ethersproject/address'
+import { Contract } from '@ethersproject/contracts'
 import { computeAddress } from '@ethersproject/transactions'
+import { mainModule } from 'process'
 
 // Commonly used test mnemonic
 const mnemonic =
@@ -16,15 +20,35 @@ const mnemonic =
 // Address derived at m/44'/60'/0'/0 of test mnemonic
 const etherbase = '0x9858EfFD232B4033E47d90003D41EC34EcaEda94'
 
+const sleep = m => new Promise(r => setTimeout(r, m))
+
+const poll = async (
+  functionToCall: Function,
+  timeout: number,
+  successCondition: Function = (res:any[]) => res.length !== 0,
+  pollInterval: number = 100
+) => {
+  for (let i = 0; i < timeout; i+=pollInterval) {
+    const res = await functionToCall()
+    if (successCondition(res)) {
+      return res
+    }
+    await sleep(pollInterval)
+  }
+}
+
 describe('Transactions', () => {
-  let l1Provider
-  let l1Signer
-  let l2Provider
-  let addressResolver
+  let l1Provider: Provider
+  let l1Signer: Wallet
+  let l2Provider: Provider
+  let addressResolver: Contract
+  let postgres: PostgresDB
 
   before(async () => {
+    postgres = new PostgresDB('postgres', 5432, 'test', 'test', 'rollup')
     l1Provider = new JsonRpcProvider('http://l1_chain:9545')
-    l1Signer = l1Provider.getSigner()
+    // l1Signer = Wallet.fromMnemonic(mnemonic).connect(l1Provider)
+    l1Signer = Wallet.createRandom().connect(l1Provider)
     const web3 = new Web3Provider(
       ganache.provider({
         mnemonic,
@@ -44,16 +68,33 @@ describe('Transactions', () => {
   })
 
   it('should allow us to get the l1ToL2TxQueueAddress', async () => {
-    const sleep = m => new Promise(r => setTimeout(r, m))
     // Set up L1ToL2TransactionQueue contract object
     const l1ToL2TransactionQueueAddress = await addressResolver.getAddress('L1ToL2TransactionQueue')
     const L1ToL2TransactionQueueFactory = getContractFactory('L1ToL2TransactionQueue')
-    const l1ToL2TransactionQueue = L1ToL2TransactionQueueFactory.connect(l1Signer).attach(l1ToL2TransactionQueueAddress)
+    const l1ToL2TransactionQueue: Contract = L1ToL2TransactionQueueFactory.connect(l1Signer).attach(l1ToL2TransactionQueueAddress)
 
     // Send an L1ToL2Transaction
     const txResponse = await l1ToL2TransactionQueue.enqueueL1ToL2Message('0x' + '01'.repeat(20), 500_000, '0x' + '00')
     const txReceipt = await txResponse.wait()
-    await sleep(1000)
+    console.log('Tx response:', txResponse)
+    console.log('Receipt:', txReceipt)
+    await sleep(5000)
+
+    // TODO: Fix Error
+
+    // // Now poll postgres until we get the value we expect!
+    // console.log('Sleeping....')
+    // console.log('Woke up!....')
+    // const l1RollupTxs = await poll(()=>postgres.select('SELECT * FROM l1_rollup_tx'), 10_000)
+
+    // // 0xaaa6c3FBA9be28B4e4777Cdcb9Ed781199083358
+    // console.log('Rollup transactions:', l1RollupTxs)
+    // console.log(l1Signer)
+    // console.log(await l1Signer.getAddress())
+    // l1RollupTxs[0].l1_message_sender.should.equal(await l1Signer.getAddress())
+
+
+    // console.log('Rollup transactions:', l1RollupTxs)
   }).timeout(100_000_000)
 })
 
